a38924c6b7c8b1ea5137af3a3155aa6e0d136341
Hiren Vadher <hirenvadher954@gmail.com>
2021-09-23 23:58:26 +0530

    message changed

--- lib/app/home/news/Notes_Page.dart
+++ lib/app/home/news/Notes_Page.dart
@@ -1,14 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gtu_question_paper/app/home/news/bloc/notes_bloc.dart';
+import 'package:gtu_question_paper/common_widgets/pdf_viewer_page.dart';
+import 'package:gtu_question_paper/repository/notes_section/firebase_notes_repository.dart';
 
 class NotesPage extends StatelessWidget {
   const NotesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Notes"),
+    Size size = MediaQuery.of(context).size;
+    return BlocProvider<NotesBloc>(
+      create: (_) {
+        return NotesBloc(notesRepository: FirebaseNotesRepository())
+          ..add(LoadNotes());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Notes")),
+        ),
+        body: BlocBuilder<NotesBloc, NotesState>(
+          builder: (context, state) {
+            if (state is! NotesLoaded) {
+              return SizedBox(
+                height: 100,
+                width: 100,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            final notesList = state.notesList;
+            return ListView.builder(
+              itemCount: notesList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 800),
+                  child: SlideAnimation(
+                    child: buildCard(
+                        context,
+                        size,
+                        notesList.elementAt(index).name,
+                        notesList.elementAt(index).pdfLink),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
+
+  Widget buildCard(context, Size size, String title, String pdfLink) {
+    return InkWell(
+      onTap: () => openPDF(context, pdfLink, title),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: size.width,
+          height: size.height * 0.1,
+          decoration: BoxDecoration(
+              gradient:
+                  LinearGradient(colors: GradientColors.facebookMessenger),
+              borderRadius: BorderRadius.circular(8)),
+          child: Center(
+            child: Text(
+              title,
+              style: GoogleFonts.robotoCondensed(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void openPDF(BuildContext context, String url, String title) =>
+      Navigator.of(context).push(
+        MaterialPageRoute(
+            builder: (context) => PDFViewerPage(url: url, title: title)),
+      );
 }
--- lib/common_widgets/pdf_viewer_page.dart
+++ lib/common_widgets/pdf_viewer_page.dart
@@ -1,4 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class PDFViewerPage extends StatefulWidget {
@@ -13,11 +15,35 @@ class PDFViewerPage extends StatefulWidget {
 
 class _PDFViewerPageState extends State<PDFViewerPage> {
   late PdfViewerController _pdfViewerController;
+  late FToast fToast;
 
   @override
   void initState() {
     _pdfViewerController = PdfViewerController();
     super.initState();
+    fToast = FToast();
+    fToast.init(context);
+    Future.delayed(Duration(seconds: 2), _showToast);
+  }
+
+  _showToast() {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+      ),
+      child: AutoSizeText(
+        "It will take some time to load the pdf because of slow internet connection",
+        maxLines: 3,
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(seconds: 5),
+    );
   }
 
   @override
